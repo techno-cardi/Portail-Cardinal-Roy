406633fe19a9b854b2b6f6f1701671b497429a52
techno-cardi <tremblay.kevin@cscapitale.qc.ca>
2026-09-14 06:58:38 -0400

    Actualiser le cache pour les gestes mobiles

--- agendakevin/sw.js
+++ agendakevin/sw.js
@@ -1,5 +1,5 @@
-const CACHE='cr-agenda-v13-20260914';
-const ASSETS=['./','./index.html','./app.css?v=20260913-3','./ui-tweaks.css?v=20260913-1','./print.css?v=20260913-1','./app.js?v=20260913-4','./course-meta.js?v=20260914-3','./planner-tools.js?v=20260913-2','./agenda-tools.js?v=20260913-1','./agenda-ui.js?v=20260914-2','./course-list.js?v=20260913-1','./google-sync-ui.js?v=20260913-2','./title-edit.js?v=20260913-2','./google-calendar-sync.gs.txt','./manifest.webmanifest','./icon.svg'];
+const CACHE='cr-agenda-v14-20260914';
+const ASSETS=['./','./index.html','./app.css?v=20260913-3','./ui-tweaks.css?v=20260913-1','./print.css?v=20260913-1','./app.js?v=20260913-4','./course-meta.js?v=20260914-3','./planner-tools.js?v=20260913-2','./agenda-tools.js?v=20260913-1','./agenda-ui.js?v=20260914-2','./course-list.js?v=20260913-1','./google-sync-ui.js?v=20260913-2','./mobile-gestures.js?v=20260914-1','./title-edit.js?v=20260913-2','./google-calendar-sync.gs.txt','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
